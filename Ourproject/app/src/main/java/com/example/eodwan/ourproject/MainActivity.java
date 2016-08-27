854f55a02b26a73d49c855213c5da557d9f6c667
enas-adwan <eodwan@eodwan.home>
2016-08-27 22:31:16 +0300

    try

--- Ourproject/app/src/main/java/com/example/eodwan/ourproject/MainActivity.java
+++ Ourproject/app/src/main/java/com/example/eodwan/ourproject/MainActivity.java
@@ -8,24 +8,52 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        signup = (Button) findViewById(R.id.signup);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+                Intent i=new Intent(MainActivity.this,SignUp.class);
+
+                startActivity(i);
+
+             /*   new SignUpActivity(this,status,role,0).execute(username,password);*/
+
+
+            }
+        });
+
+    /*   FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
     }
 
     @Override
