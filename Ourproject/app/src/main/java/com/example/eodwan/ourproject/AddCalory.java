7a52181bfefe1efb74a70783808c7165ee348dbc
dianakh <diana_k_2012@hotmail.com>
2016-09-04 22:06:21 +0300

    try

--- Ourproject/app/src/main/java/com/example/eodwan/ourproject/AddCalory.java
+++ Ourproject/app/src/main/java/com/example/eodwan/ourproject/AddCalory.java
@@ -12,10 +12,23 @@ package com.example.eodwan.ourproject;
         import android.widget.ListView;
         import android.widget.TextView;
         import android.widget.Toast;
+
+        import java.util.ArrayList;
+        import java.util.List;
+        import android.app.Activity;
+        import android.os.Bundle;
+
+        import android.view.View.OnClickListener;
+        import android.widget.ArrayAdapter;
+        import android.widget.Button;
+        import android.widget.Spinner;
+        import android.widget.Toast;
+
 /**
  * Created by hp on 03/09/2016.
  */
 public class AddCalory extends AppCompatActivity  {
+        private Spinner spinner1;
         EditText calory_num;
         Context context=this;
         ListView listView;
@@ -29,6 +42,7 @@ public class AddCalory extends AppCompatActivity  {
                 super.onCreate(savedInstanceState);
                 setContentView(R.layout.calory);
                 calory_num=(EditText)findViewById(R.id.edit1);
+                spinner1 = (Spinner) findViewById(R.id.spinner1);
 
         }
 
@@ -37,6 +51,23 @@ public class AddCalory extends AppCompatActivity  {
                 float num=Float.valueOf(calory_num.getText().toString());
                 userDbHelper=new  recipeDbHelper(context);
                 sqLiteDatabase=userDbHelper.getWritableDatabase();
+               String quintity= String.valueOf(spinner1.getSelectedItem());
+                switch(quintity){
+
+                        case "Spoon":
+                                num=num/6.666666666666667f;
+                                break;
+                    case "Cup":
+                        num=num*1.1f;
+                        break;
+                        case "1/2 Cup":
+                               num=num*.6f;
+                                break;
+                        case "3/4 Cup": /** AlerDialog when click on Exit */
+                                num=num*.85f;
+                                break;
+
+                }
                 userDbHelper.addinnformation(num,sqLiteDatabase);
                 Toast.makeText(getBaseContext(),"Data saved",Toast.LENGTH_LONG).show();
                 userDbHelper.close();
